d634892ced39afd828661e99cbc4fe9c6581e266
Andre Palhinha <andre.palhinha@gmail.com>
2020-09-03 11:37:02 +0100

    rebuild

--- content/maintenance/extrafiles.md
+++ content/maintenance/extrafiles.md
@@ -4,7 +4,7 @@ date: 2020-07-11T09:11:42+01:00
 draft: false
 ---
 
-Listing of exercise files not coming from the database.
+Listing of exercise files not coming from the database:
 
 <!--more-->
 
--- hdelete-build.bat
+++ hdelete-build.bat
@@ -1,26 +1,9 @@
 copy docs\CNAME .
-pause
-
 cd docs
-pause
-
 rm -R *
-pause
-
 cd ..
-pause
-
 copy CNAME docs
-pause
-
 hugo
-pause
-
 git add .
-pause
-
 git commit -m "rebuild"
-pause
-
 git push origin master
-pause
